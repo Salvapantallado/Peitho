5ef4f0271cf14d39b2bb718867a33575dae6ebd1
Lighuen <lighu.miranda@gmail.com>
2022-08-30 00:50:48 -0300

    Ticker and main img update

--- peitho/src/components/home-components/MainImage.jsx
+++ peitho/src/components/home-components/MainImage.jsx
@@ -1,12 +1,16 @@
 import "../../styles/mainImage.css";
+import Banner1 from "../../images/Home/banner1.jpeg"
 
 export default function MainImage() {
   return (
 
       <div className="main-img_text">
+        <div className="banner-text">
+
         <h1>Bienvenido a Peitho!</h1>
         <h2>Ropa hecha a mano, a tu medida!</h2>
         <h3>Con amor y dedicación </h3>
+        </div>
       </div>
 
   );
