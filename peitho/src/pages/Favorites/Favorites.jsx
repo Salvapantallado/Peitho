963aa7ec62cb84ee09c469d055bc4be4c0cbb05f
Lighuen <lighu.miranda@gmail.com>
2022-11-08 23:30:23 -0300

    Fav fix 4

--- peitho/src/pages/Favorites/Favorites.jsx
+++ peitho/src/pages/Favorites/Favorites.jsx
@@ -136,12 +136,14 @@ export default function Favorites() {
   };
 
   function PriceMix() {
-    if (localFavorites.length && localFavorites !== null) {
-      const testing = localFavorites.map((x) => x.price * x.product_qty);
-      const testingSum = testing.reduce((a, b) => a + b);
-      return '$' + testingSum;
-    }
-    return;
+    // if (localFavorites && localFavorites !== null) {
+      function Mixing() {
+        const testing = localFavorites.map((x) => x.price * x.product_qty);
+        const testingSum = testing.reduce((a, b) => a + b);
+        return '$' + testingSum;
+      }
+    
+    localFavorites && localFavorites !== null ? Mixing() : null 
   }
 
   return (
